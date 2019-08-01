a90b5e4270a0ad926f048a952409d7fca1c88e5f
saramarin23 <saramarin.econ@gmail.com>
2019-08-01 20:53:18 +0200

    fix reset and preview functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -115,7 +115,9 @@ ratioPalette3.addEventListener("change", selectPalette3);
 
 //collapsible
 
-const collapsibleTriggers = document.querySelectorAll(".js-collapsible__trigger");
+const collapsibleTriggers = document.querySelectorAll(
+  ".js-collapsible__trigger"
+);
 
 function updateCollapsible(event) {
   const currentCollapsible = event.currentTarget.parentElement;
@@ -133,3 +135,34 @@ function updateCollapsible(event) {
 for (const item of collapsibleTriggers) {
   item.addEventListener("click", updateCollapsible);
 }
+
+//botón reset
+const reset = document.querySelector(".js-sectiona__buton");
+function resetAutopreview() {
+  const previewName = document.querySelector(".js-box1_result");
+  const previewJob = document.querySelector(".js-preview__job");
+  previewName.innerHTML = "Nombre Apellido";
+  previewJob.innerHTML = "Front-end developer";
+}
+function deleteText() {
+  const image = document.querySelector(".js-card__image");
+  const formFields = document.querySelector(".js-form");
+  const preview = document.querySelector(".js-preview");
+  formFields.reset(); //Reseteamos el formulario
+  preview.src = " "; //Dejamos en blanco para que no meta imagen
+  image.style.backgroundImage = "url(../../assets/images/default.jpg)";
+}
+reset.addEventListener("click", deleteText);
+reset.addEventListener("click", resetAutopreview);
+
+// imagen preview
+const browse = document.querySelector(".js-form__photo");
+const loadFile = function(event) {
+  let preview = document.querySelector(".preview");
+  let cardImage = document.querySelector(".js-card__image");
+  preview.src = URL.createObjectURL(event.target.files[0]);
+  cardImage.style.backgroundImage = `url(${URL.createObjectURL(
+    event.target.files[0]
+  )})`;
+};
+browse.addEventListener("change", loadFile);
