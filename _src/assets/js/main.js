969218a62fc6521b460904ce60bb0d181dc5a491
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-08-03 10:42:01 +0200

    merging current master with palettes function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,156 +1,82 @@
 "use strict";
-debugger;
+
+// autopreview function
 
 autoPreview(".js-input-name", ".preview__bio--name", "Nombre Apellido");
 autoPreview(".js-input-job", ".preview__bio--job", "Trabajo");
 
 function autoPreview(variableinput, variableoutput, defaultValue) {
-  const outputText = document.querySelector(variableoutput);
-  const inputText = document.querySelector(variableinput);
+    const outputText = document.querySelector(variableoutput);
+    const inputText = document.querySelector(variableinput);
 
-  function changePara(event) {
-    const inputValue = event.currentTarget;
-    if (inputText.value) {
-      outputText.innerHTML = inputValue.value;
-    } else {
-      outputText.innerHTML = defaultValue;
-      console.log(defaultValue);
+    function changePara(event) {
+        const inputValue = event.currentTarget;
+        if (inputText.value) {
+            outputText.innerHTML = inputValue.value;
+        } else {
+            outputText.innerHTML = defaultValue;
+            console.log(defaultValue);
+        }
     }
-  }
-  inputText.addEventListener("keyup", changePara);
+    inputText.addEventListener("keyup", changePara);
 }
 
-// //
+// change palettes function
 
-// definir los colores, seleccionados y resultados
+const previewCard = document.querySelector(".js-card");
 
-const color1Choosed = document.querySelector(".js-box__color1");
-const color2Choosed = document.querySelector(".js-box__color2");
-// const color3Choosed = document.querySelector(".js-box__color3");
-//
-const color4Choosed = document.querySelector(".js-box__color4");
-const color5Choosed = document.querySelector(".js-box__color5");
-// const color6Choosed = document.querySelector(".js-box__color6");
-//
-const color7Choosed = document.querySelector(".js-box__color7");
-const color8Choosed = document.querySelector(".js-box__color8");
-// const color9Choosed = document.querySelector(".js-box__color9");
-
-//
-const color1Result = document.querySelector(".js-box1_result");
-const color2Result = document.querySelector(".js-box2_result");
-// const color3Result = document.querySelector(".js-box3_result");
-
-//funciones seleccionar
-
-function selectPalette1(event) {
-  console.log(event.target);
-  changePalette1(color1Result, color2Result); //color3Result);
+function createPaletteSelectorFunction(classPalette) {
+    return function() {
+        previewCard.className = `js-card ${classPalette}`;
+    };
 }
 
-function selectPalette2(event) {
-  console.log(event.target);
-  changePalette2(color1Result, color2Result); //color3Result);
-}
+const selectPalette1 = createPaletteSelectorFunction("palette1");
+const selectPalette2 = createPaletteSelectorFunction("palette2");
+const selectPalette3 = createPaletteSelectorFunction("palette3");
 
-function selectPalette3(event) {
-  console.log(event.target);
-  changePalette3(color1Result, color2Result); // color3Result
-}
-
-//funcion cambiar paletas
-
-function changePalette1(color1Result, color2Result) {
-  //color3Result
-  // color1Result.classList.remove("js-box__color4");
-  // color2Result.classList.remove("js-box__color5");
-  // color3Result.classList.remove("js-box__color6");
-  color1Result.classList.remove("js-box__color4");
-  color2Result.classList.remove("js-box__color5");
-  // color3Result.classList.remove("js-box__color6");
-  color1Result.classList.remove("js-box__color7");
-  color2Result.classList.remove("js-box__color8");
-  // color3Result.classList.remove("js-box__color9");
-  color1Result.classList.add("js-box__color1");
-  color2Result.classList.add("js-box__color2");
-  // color3Result.classList.add("js-box__color3");
-}
-
-function changePalette2(color4Result, color5Result) {
-  //color6Result
-
-  color1Result.classList.remove("js-box__color7");
-  color2Result.classList.remove("js-box__color8");
-  // color3Result.classList.remove("js-box__color9");
-  color1Result.classList.remove("js-box__color1");
-  color2Result.classList.remove("js-box__color2");
-  // color3Result.classList.remove("js-box__color3");
-  color4Result.classList.add("js-box__color4");
-  color5Result.classList.add("js-box__color5");
-  // color6Result.classList.add("js-box__color6");
-}
-
-function changePalette3(color7Result, color8Result) {
-  // color9Result
-  color1Result.classList.remove("js-box__color1");
-  color2Result.classList.remove("js-box__color2");
-  // color3Result.classList.remove("js-box__color3");
-  color1Result.classList.remove("js-box__color4");
-  color2Result.classList.remove("js-box__color5");
-  // color3Result.classList.remove("js-box__color6");
-  color7Result.classList.add("js-box__color7");
-  color8Result.classList.add("js-box__color8");
-  // color9Result.classList.add("js-box__color9");
-}
-
-// listeners
-const ratioPalette1 = document.querySelector("#option1");
-ratioPalette1.addEventListener("change", selectPalette1);
-
-const ratioPalette2 = document.querySelector("#option2");
-ratioPalette2.addEventListener("change", selectPalette2);
-
-const ratioPalette3 = document.querySelector("#option3");
-ratioPalette3.addEventListener("change", selectPalette3);
+const ratio1 = document.querySelector("#option1").addEventListener("change", selectPalette1);
+const ratio2 = document.querySelector("#option2").addEventListener("change", selectPalette2);
+const ratio3 = document.querySelector("#option3").addEventListener("change", selectPalette3);
 
 //collapsible
 
-const collapsibleTriggers = document.querySelectorAll(
-  ".js-collapsible__trigger"
-);
+const collapsibleTriggers = document.querySelectorAll(".js-collapsible__trigger");
 
 function updateCollapsible(event) {
-  const currentCollapsible = event.currentTarget.parentElement;
+    const currentCollapsible = event.currentTarget.parentElement;
 
-  if (currentCollapsible.classList.contains("js-collapsible--open")) {
-    currentCollapsible.classList.remove("js-collapsible--open");
-  } else {
-    for (const item of collapsibleTriggers) {
-      item.parentElement.classList.remove("js-collapsible--open");
+    if (currentCollapsible.classList.contains("js-collapsible--open")) {
+        currentCollapsible.classList.remove("js-collapsible--open");
+    } else {
+        for (const item of collapsibleTriggers) {
+            item.parentElement.classList.remove("js-collapsible--open");
+        }
+        currentCollapsible.classList.add("js-collapsible--open");
     }
-    currentCollapsible.classList.add("js-collapsible--open");
-  }
 }
 
 for (const item of collapsibleTriggers) {
-  item.addEventListener("click", updateCollapsible);
+    item.addEventListener("click", updateCollapsible);
 }
 
 //botón reset
 const reset = document.querySelector(".js-sectiona__buton");
+
 function resetAutopreview() {
-  const previewName = document.querySelector(".js-box1_result");
-  const previewJob = document.querySelector(".js-preview__job");
-  previewName.innerHTML = "Nombre Apellido";
-  previewJob.innerHTML = "Front-end developer";
+    const previewName = document.querySelector(".js-box1_result");
+    const previewJob = document.querySelector(".js-preview__job");
+    previewName.innerHTML = "Nombre Apellido";
+    previewJob.innerHTML = "Front-end developer";
 }
+
 function deleteText() {
-  const image = document.querySelector(".js-card__image");
-  const formFields = document.querySelector(".js-form");
-  const preview = document.querySelector(".js-preview");
-  formFields.reset(); //Reseteamos el formulario
-  preview.src = " "; //Dejamos en blanco para que no meta imagen
-  image.style.backgroundImage = "url(../../assets/images/default.jpg)";
+    const image = document.querySelector(".js-card__image");
+    const formFields = document.querySelector(".js-form");
+    const preview = document.querySelector(".js-preview");
+    formFields.reset(); //Reseteamos el formulario
+    preview.src = " "; //Dejamos en blanco para que no meta imagen
+    image.style.backgroundImage = "url(../../assets/images/default.jpg)";
 }
 reset.addEventListener("click", deleteText);
 reset.addEventListener("click", resetAutopreview);
@@ -158,11 +84,9 @@ reset.addEventListener("click", resetAutopreview);
 // imagen preview
 const browse = document.querySelector(".js-form__photo");
 const loadFile = function(event) {
-  let preview = document.querySelector(".preview");
-  let cardImage = document.querySelector(".js-card__image");
-  preview.src = URL.createObjectURL(event.target.files[0]);
-  cardImage.style.backgroundImage = `url(${URL.createObjectURL(
-    event.target.files[0]
-  )})`;
+    let preview = document.querySelector(".preview");
+    let cardImage = document.querySelector(".js-card__image");
+    preview.src = URL.createObjectURL(event.target.files[0]);
+    cardImage.style.backgroundImage = `url(${URL.createObjectURL(event.target.files[0])})`;
 };
 browse.addEventListener("change", loadFile);
