e4b4a41de0383d95daa01bb64dd4620d0776e737
Raquel MM <mendizraquel@gmail.com>
2019-08-04 13:23:33 +0200

    Add js typography function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,19 +6,19 @@ autoPreview(".js-input-name", ".preview__bio--name", "Nombre Apellido");
 autoPreview(".js-input-job", ".preview__bio--job", "Trabajo");
 
 function autoPreview(variableinput, variableoutput, defaultValue) {
-    const outputText = document.querySelector(variableoutput);
-    const inputText = document.querySelector(variableinput);
+  const outputText = document.querySelector(variableoutput);
+  const inputText = document.querySelector(variableinput);
 
-    function changePara(event) {
-        const inputValue = event.currentTarget;
-        if (inputText.value) {
-            outputText.innerHTML = inputValue.value;
-        } else {
-            outputText.innerHTML = defaultValue;
-            console.log(defaultValue);
-        }
+  function changePara(event) {
+    const inputValue = event.currentTarget;
+    if (inputText.value) {
+      outputText.innerHTML = inputValue.value;
+    } else {
+      outputText.innerHTML = defaultValue;
+      console.log(defaultValue);
     }
-    inputText.addEventListener("keyup", changePara);
+  }
+  inputText.addEventListener("keyup", changePara);
 }
 
 // change palettes function
@@ -26,9 +26,9 @@ function autoPreview(variableinput, variableoutput, defaultValue) {
 const previewCard = document.querySelector(".js-card");
 
 function createPaletteSelectorFunction(classPalette) {
-    return function() {
-        previewCard.className = `js-card ${classPalette}`;
-    };
+  return function() {
+    previewCard.className = `js-card ${classPalette}`;
+  };
 }
 
 const selectPalette1 = createPaletteSelectorFunction("palette1");
@@ -39,50 +39,68 @@ const ratio1 = document.querySelector("#option1").addEventListener("change", sel
 const ratio2 = document.querySelector("#option2").addEventListener("change", selectPalette2);
 const ratio3 = document.querySelector("#option3").addEventListener("change", selectPalette3);
 
+// change typography function
+
+const previewCardTypo = document.querySelector(".js-card");
+
+function createTypographySelectorFunction(classTypography) {
+  return function() {
+    previewCardTypo.className = `js-card ${classTypography}`;
+  };
+}
+
+const selectTypography1 = createTypographySelectorFunction("typography1");
+const selectTypography2 = createTypographySelectorFunction("typography2");
+const selectTypography3 = createTypographySelectorFunction("typography3");
+
+const ratioTypo1 = document.querySelector("#option1_typo").addEventListener("change", selectTypography1);
+const ratioTypo2 = document.querySelector("#option2_typo").addEventListener("change", selectTypography2);
+const ratioTypo3 = document.querySelector("#option3_typo").addEventListener("change", selectTypography3);
+
 //collapsible
 
 const collapsibleTriggers = document.querySelectorAll(".js-collapsible__trigger");
 
 function updateCollapsible(event) {
-    const currentCollapsible = event.currentTarget.parentElement;
+  const currentCollapsible = event.currentTarget.parentElement;
 
-    if (currentCollapsible.classList.contains("js-collapsible--open")) {
-        currentCollapsible.classList.remove("js-collapsible--open");
-    } else {
-        for (const item of collapsibleTriggers) {
-            item.parentElement.classList.remove("js-collapsible--open");
-        }
-        currentCollapsible.classList.add("js-collapsible--open");
+  if (currentCollapsible.classList.contains("js-collapsible--open")) {
+    currentCollapsible.classList.remove("js-collapsible--open");
+  } else {
+    for (const item of collapsibleTriggers) {
+      item.parentElement.classList.remove("js-collapsible--open");
     }
+    currentCollapsible.classList.add("js-collapsible--open");
+  }
 }
 
 for (const item of collapsibleTriggers) {
-    item.addEventListener("click", updateCollapsible);
+  item.addEventListener("click", updateCollapsible);
 }
 
 //botón reset
 const reset = document.querySelector(".js-sectiona__buton");
 
 function resetAutopreview() {
-    const previewName = document.querySelector(".js-box1_result");
-    const previewJob = document.querySelector(".js-preview__job");
-    const iconsUsed = document.querySelectorAll(".js-icon-preview");
+  const previewName = document.querySelector(".js-box1_result");
+  const previewJob = document.querySelector(".js-preview__job");
+  const iconsUsed = document.querySelectorAll(".js-icon-preview");
 
-    previewName.innerHTML = "Nombre Apellido";
-    previewJob.innerHTML = "Front-end developer";
+  previewName.innerHTML = "Nombre Apellido";
+  previewJob.innerHTML = "Front-end developer";
 
-    for (const iconUsed of iconsUsed) {
-        iconUsed.style.color = "lightgrey";
-    }
+  for (const iconUsed of iconsUsed) {
+    iconUsed.style.color = "lightgrey";
+  }
 }
 
 function deleteText() {
-    const image = document.querySelector(".js-card__image");
-    const formFields = document.querySelector(".js-form");
-    const preview = document.querySelector(".js-preview");
-    formFields.reset(); //Reseteamos el formulario
-    preview.src = " "; //Dejamos en blanco para que no meta imagen
-    image.style.backgroundImage = "url(../../assets/images/default.jpg)";
+  const image = document.querySelector(".js-card__image");
+  const formFields = document.querySelector(".js-form");
+  const preview = document.querySelector(".js-preview");
+  formFields.reset(); //Reseteamos el formulario
+  preview.src = " "; //Dejamos en blanco para que no meta imagen
+  image.style.backgroundImage = "url(../../assets/images/default.jpg)";
 }
 
 reset.addEventListener("click", deleteText);
@@ -91,27 +109,27 @@ reset.addEventListener("click", resetAutopreview);
 // imagen preview
 const browse = document.querySelector(".js-form__photo");
 const loadFile = function(event) {
-    let preview = document.querySelector(".preview");
-    let cardImage = document.querySelector(".js-card__image");
-    preview.src = URL.createObjectURL(event.target.files[0]);
-    cardImage.style.backgroundImage = `url(${URL.createObjectURL(event.target.files[0])})`;
+  let preview = document.querySelector(".preview");
+  let cardImage = document.querySelector(".js-card__image");
+  preview.src = URL.createObjectURL(event.target.files[0]);
+  cardImage.style.backgroundImage = `url(${URL.createObjectURL(event.target.files[0])})`;
 };
 browse.addEventListener("change", loadFile);
 
 // icon changes with info & reset in case there aren't inputs
 
 function changeIconColor(variableinput, classInput, classIcon) {
-    const classIconUsed = document.querySelector(classIcon);
-    const inputText = document.querySelector(variableinput);
+  const classIconUsed = document.querySelector(classIcon);
+  const inputText = document.querySelector(variableinput);
 
-    const handle = function() {
-        if (inputText.value) {
-            classIconUsed.style.color = "#114e4e";
-        } else {
-            classIconUsed.style.color = "lightgrey";
-        }
-    };
-    document.querySelector(classInput).addEventListener("keyup", handle);
+  const handle = function() {
+    if (inputText.value) {
+      classIconUsed.style.color = "#114e4e";
+    } else {
+      classIconUsed.style.color = "lightgrey";
+    }
+  };
+  document.querySelector(classInput).addEventListener("keyup", handle);
 }
 
 changeIconColor(".js-input-phone", "#phone", ".js-icon-phone");
@@ -122,17 +140,17 @@ changeIconColor(".js-input-linkedin", "#linkedin", ".js-icon-linkedin");
 // icon clickables when you write email,github & linkedin
 
 function changeLinkIcon(variableinput, classIcon) {
-    const classIconUsed = document.querySelector(classIcon);
-    const inputText = document.querySelector(variableinput);
+  const classIconUsed = document.querySelector(classIcon);
+  const inputText = document.querySelector(variableinput);
 
-    const handle = function() {
-        if (inputText.value.startsWith("http://") || inputText.value.startsWith("https://")) {
-            classIconUsed.href = inputText.value;
-        } else {
-            classIconUsed.href = `http://${inputText.value}`;
-        }
-    };
-    document.querySelector(variableinput).addEventListener("keyup", handle);
+  const handle = function() {
+    if (inputText.value.startsWith("http://") || inputText.value.startsWith("https://")) {
+      classIconUsed.href = inputText.value;
+    } else {
+      classIconUsed.href = `http://${inputText.value}`;
+    }
+  };
+  document.querySelector(variableinput).addEventListener("keyup", handle);
 }
 
 changeLinkIcon(".js-input-email", ".js-icon-link-email");
